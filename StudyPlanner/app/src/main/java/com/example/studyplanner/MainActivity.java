63f3dc3705f4d9ed2ce489ab33b8dd7d2e8d5f28
kiet3123 <165416763+kiet3123@users.noreply.github.com>
2025-10-20 13:08:28 +0700

    Update MainActivity.java

--- StudyPlanner/app/src/main/java/com/example/studyplanner/MainActivity.java
+++ StudyPlanner/app/src/main/java/com/example/studyplanner/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.studyplanner;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+        createNotificationChannel();
 
         setContentView(R.layout.activity_main);
 
@@ -52,5 +57,17 @@ public class MainActivity extends AppCompatActivity {
         }
 
         navController.setGraph(graph);
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
