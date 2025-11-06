c71a565a9bd0ebb5854b868387738a857ff8be39
bahungTDTU <161673407+bahungTDTU@users.noreply.github.com>
2025-11-06 09:44:34 +0700

    Update SubjectHours.java

--- StudyPlanner/app/src/main/java/com/example/studyplanner/data/model/SubjectHours.java
+++ StudyPlanner/app/src/main/java/com/example/studyplanner/data/model/SubjectHours.java
@@ -3,7 +3,7 @@ package com.example.studyplanner.data.model;
 public class SubjectHours {
     public final String subjectName;
     public final String formattedHours;
-    public final long totalMillis; // We need this for the chart
+    public final long totalMillis;
 
     public SubjectHours(String subjectName, String formattedHours, long totalMillis) {
         this.subjectName = subjectName;
